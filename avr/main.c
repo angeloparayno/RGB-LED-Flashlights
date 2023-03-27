07623ebacedc8c384d195d9312338940156ce6a1
Angelo Parayno <angeloparayno.dev@gmail.com>
2023-03-27 16:56:14 -0700

    Update main.c

--- avr/main.c
+++ avr/main.c
@@ -21,32 +21,32 @@ int main(void)
         	if (PIND & (1 << PD0)) 
 		{   
 			// is SWITCH_1 open?
-            		PORTD |= (1 << PD3);   // switch is closed, switch RED LED on
-			//PORTD |= (1 << PD5);   // switch is closed, switch BLUE LED on
+            		PORTD |= (1 << PD3);   	 // switch is closed, switch RED LED on
+			//PORTD |= (1 << PD5);   // switch is closed, switch BLUE LED on (Remove for Magenta)
         	}
         	else 
 		{
-			PORTD &= ~(1 << PD3);  // switch is open, switch RED LED off
-			//PORTD &= ~(1 << PD5);  // switch is open, switch BLUE LED off
+			PORTD &= ~(1 << PD3);   // switch is open, switch RED LED off
+			//PORTD &= ~(1 << PD5); // switch is open, switch BLUE LED off
         	}
 		
 		//GREEN AND YELLOW
         	if (PIND & (1 << PD1)) 
 		{   	
 			// is SWITCH_2 open?
-            		PORTD |= (1 << PD4);   // switch is closed, switch GREEN LED on
-			//PORTD |= (1 << PD3);   // switch is closed, switch RED LED on
+            		PORTD |= (1 << PD4);    // switch is closed, switch GREEN LED on
+			//PORTD |= (1 << PD3);  // switch is closed, switch RED LED on (Remove comment for Yellow)
         	}
         	else 
 		{
-			PORTD &= ~(1 << PD4);  // switch is open, switch GREEN LED off
-			//PORTD &= ~(1 << PD3);  // switch is open, switch RED LED off
+			PORTD &= ~(1 << PD4);   // switch is open, switch GREEN LED off
+			//PORTD &= ~(1 << PD3); // switch is open, switch RED LED off 
         	}  
 		//BLUE AND CYAN
         	if (PIND & (1 << PD2)) 
 		{   // is SWITCH_2 open?
             		PORTD |= (1 << PD5);   // switch is closed, switch BLUE LED on
-			//PORTD |= (1 << PD4);   // switch is closed, switch GREEN LED on
+			//PORTD |= (1 << PD4);   // switch is closed, switch GREEN LED on (Remove comment for Cyan)
         	}
         	else 
 		{
